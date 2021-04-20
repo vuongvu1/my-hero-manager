ed16af57678fdfd86c2273dbc156e3df67a71217
vuongvu <vuong.vu@2359media.com>
2021-04-20 23:43:59 +0800

    Update script

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 const fs = require("fs");
 const creds = require("./credentials.json");
 
-var doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
+const writeContent = (rowData) => {
+  const content = `
+    const Heroes = ${JSON.stringify(rowData)};
+    export default Heroes;
+  `;
+
+  console.log("Writing to file...");
+
+  fs.writeFile("output/index.js", content, function (err) {
+    if (err) return console.log(err);
+  });
+
+  console.log("DONE!");
+};
 
 const main = async () => {
+  const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEET_ID);
   await doc.useServiceAccountAuth(creds);
   await doc.loadInfo();
   console.log(`Sheet: ${doc.title}`);
@@ -21,18 +35,7 @@ const main = async () => {
     "Attack type": row["Attack type"],
   }));
 
-  const content = `
-const Heroes = ${JSON.stringify(formattedRows)};
-export default Heroes;
-  `;
-
-  console.log("Writing to file...");
-
-  fs.writeFile("output/index.js", content, function (err) {
-    if (err) return console.log(err);
-  });
-
-  console.log("DONE!");
+  writeContent(formattedRows);
 };
 
 main();
